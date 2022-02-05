b7a9f2b2507f728cfef1fc060f194017b725a3ee
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 21:13:08 +0600

    gradeing system

---
+++ grading.js
@@ -0,0 +1,130 @@
+var alia = 95;
+var dalia = 66;
+var salia = 40;
+var malia = 59;
+var lilia = 47;
+var jalaiya = 77;
+
+// alia grade
+if(alia >= 90){
+    console.log("alia grade A+");
+}
+else if (alia >= 80 && alia < 90){
+    console.log("alia grade A");
+}
+else if (alia >= 70 && alia < 80){
+    console.log("alia grade B");
+}
+else if (alia >= 60 && alia < 70){
+    console.log("alia grade C");
+}
+else if (alia >= 50 && alia < 60){
+    console.log("alia grade D");
+}
+else if (alia < 50){
+    console.log("alia grade F");
+}
+
+
+// dalia grade
+if (dalia >= 90){
+    console.log("dalia grade A+");
+}
+else if (dalia >= 80 && dalia < 90){
+    console.log("dalia grade A");
+}
+else if (dalia >= 70 && dalia < 80){
+    console.log("dalia grade B");
+}
+else if (dalia >= 60 && dalia < 70){
+    console.log("dalia grade C");
+}
+else if (dalia >= 50 && dalia < 60){
+    console.log("dalia grade D");
+}
+else if (dalia < 50){
+    console.log("dalia grade F");
+}
+
+
+// salia grade
+if (salia >= 90){
+    console.log("salia grade A+");
+}
+else if (salia >= 80 && salia < 90){
+    console.log("salia grade A");
+}
+else if (salia >= 70 && salia < 80){
+    console.log("salia grade B");
+}
+else if (salia >= 60 && salia < 70){
+    console.log("salia grade C");
+}
+else if (salia >= 50 && salia < 60){
+    console.log("salia grade D");
+}
+else if (salia < 50){
+    console.log("salia grade F");
+}
+
+
+// malia grade
+if (malia >= 90){
+    console.log("malia grade A+");
+}
+else if (malia >= 80 && malia < 90){
+    console.log("malia grade A");
+}
+else if (malia >= 70 && malia < 80){
+    console.log("malia grade B");
+}
+else if (malia >= 60 && malia < 70){
+    console.log("malia grade C");
+}
+else if (malia >= 50 && malia < 60){
+    console.log("malia grade D");
+}
+else if (malia < 50){
+    console.log("malia grade F");
+}
+
+// lilia grade
+if(lilia >= 90){
+    console.log("lilia grade A+");
+}
+else if (lilia >= 80 && lilia < 90){
+    console.log("lilia grade A");
+}
+else if (lilia >= 70 && lilia < 80){
+    console.log("lilia grade B");
+}
+else if (lilia >= 60 && lilia < 70){
+    console.log("lilia grade C");
+}
+else if (lilia >= 50 && lilia < 60){
+    console.log("lilia grade D");
+}
+else if (lilia < 50){
+    console.log("lilia grade F");
+}
+
+
+// jalaiya grade
+if(jalaiya >= 90){
+    console.log("jalaiya grade A+");
+}
+else if (jalaiya >= 80 && jalaiya < 90){
+    console.log("jalaiya grade A");
+}
+else if (jalaiya >= 70 && jalaiya < 80){
+    console.log("jalaiya grade B");
+}
+else if (jalaiya >= 60 && jalaiya < 70){
+    console.log("jalaiya grade C");
+}
+else if (jalaiya >= 50 && jalaiya < 60){
+    console.log("jalaiya grade D");
+}
+else if (jalaiya < 50){
+    console.log("jalaiya grade F");
+}
