da6c55d8b878b9b5f2a520fa9b9742980e48a5ec
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 21:28:03 +0600

    signal system js

---
+++ signal.js
@@ -0,0 +1,14 @@
+var signal = ["green", "red", "yellow"];
+var green = true;
+var yellow = false;
+var red = false;
+
+if(green == true){
+    console.log("You should cross the road");
+}
+else if(yellow == true){
+    console.log("You should stop");
+}
+else if(red == true){
+    console.log("In this case, if you try to cross the road, it can be dangerous");
+}
