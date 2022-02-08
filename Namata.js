f01d5634c1cd9467a4b22a3cf8fc2d83028aca76
Ezabulbary <ezabul.bary@gmail.com>
2022-02-08 22:34:34 +0600

    namata added

---
+++ Namata.js
@@ -0,0 +1,35 @@
+function namata(num, num1){
+    // console.log(num, num1);
+    var multiply = num * num1; 
+    return multiply;
+}
+
+var thirteenOne = namata(13, 1);
+console.log(thirteenOne);
+
+var thirteenTwo = namata(13, 2);
+console.log(thirteenTwo);
+
+var thirteenThree = namata(13, 3);
+console.log(thirteenThree);
+
+var thirteenFour = namata(13, 4);
+console.log(thirteenFour);
+
+var thirteenFive = namata(13, 5);
+console.log(thirteenFive);
+
+var thirteenSix = namata(13, 6);
+console.log(thirteenSix);
+
+var thirteenSeven = namata(13, 7);
+console.log(thirteenSeven);
+
+var thirteenEight = namata(13, 8);
+console.log(thirteenEight);
+
+var thirteenNine = namata(13, 9);
+console.log(thirteenNine);
+
+var thirteenTen = namata(13, 10);
+console.log(thirteenTen);
